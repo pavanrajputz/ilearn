a56d5428622216007244bd4a90bf8c9bf0a8452a
pavanrajputz <pr7676150@gmail.com>
2026-09-10 22:39:53 +0530

    establishing the project

---
+++ src/main/java/in/devigo/ilearn/IlearnApplication.java
@@ -0,0 +1,13 @@
+package in.devigo.ilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/devigo/ilearn/IlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package in.devigo.ilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
