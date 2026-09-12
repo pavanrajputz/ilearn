6f1d7b5d96894540583036f29b23be516f568c92
pavanrajputz <pr7676150@gmail.com>
2026-09-12 22:33:38 +0530

    added jwt dependencies and than wrote the jwtService after that added some details in properties

--- src/main/java/in/devigo/ilearn/security/JwtService.java
+++ src/main/java/in/devigo/ilearn/security/JwtService.java
@@ -1,4 +1,75 @@
 package in.devigo.ilearn.security;
 
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    private final SecretKey secretKey;
+    private final long expiration;
+
+    public JwtService(@Value("${jwt.secret}") String secret,
+                      @Value("${jwt.expiration}") long expiration){
+        this.secretKey = Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(secret)
+        );
+
+        this.expiration = expiration;
+    }
+
+    //generate token method via jwt
+    public String generateToken(CustomUserDetails userDetails){
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .claim("userId", userDetails.getUserId())
+                .claim("role",
+                        userDetails.getAuthorities()
+                                .iterator()
+                                .next()
+                                .getAuthority())
+                .issuedAt(new Date())
+                .expiration(
+                        new Date(System.currentTimeMillis() + expiration)
+                )
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return extractAllClaims(token)
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token,
+                                UserDetails userDetails){
+        String userName = extractUsername(token);
+        return userName.equals(userDetails.getUsername())
+            && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractAllClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
 }
